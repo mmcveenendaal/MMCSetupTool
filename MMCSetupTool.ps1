2a87e307b031d5491a68d0c5044660f69512ebaa
matsn0w <bartscholtus@gmail.com>
2022-04-30 12:56:19 +0200

    wait for wifi connection to set up

--- MMCSetupTool.ps1
+++ MMCSetupTool.ps1
@@ -1,5 +1,5 @@
 # some global vars
-$version = '1.6.5'
+$version = '1.6.6'
 $internet = $false
 
 # check for admin rights
@@ -351,6 +351,24 @@ function Connect-Wifi {
     # install the profile
     netsh wlan add profile filename=$wlanprofile | Out-Null
     netsh wlan connect name="MMC_Guest" | Out-Null
+
+    function Get-Connection {
+        return Get-NetRoute | Where-Object DestinationPrefix -eq '0.0.0.0/0' | Get-NetIPInterface | Where-Object ConnectionState -eq 'Connected'
+    }
+
+    $connected = $false
+    $count = 0
+
+    while (!$connected) {
+        if ($count++ -eq 5) {
+            return $false
+        }
+
+        $connected = Get-Connection
+        Start-Sleep -Seconds 3
+    }
+
+    return $true
 }
 
 # check the activation status of Windows
