5f96fdc16c737b2e245770b8ebe7ac489e871bb6
L. Beijaard <lars.beijaard@outlook.com>
2023-07-10 17:44:02 +0200

    feat(gui): update label

--- MMCSetupToolGUI.ps1
+++ MMCSetupToolGUI.ps1
@@ -38,6 +38,7 @@ function Show-AdministratorStatus {
     $Status.Location = New-Object Point(150, 30)
 }
 
+
 function Show-NetworkConnectionStatus {
     $Title = New-Label -Text "Internetverbinding: "
     $Title.Location = New-Object Point(250, 30)
